87e594533875fc1ed3447ecb63b5c5dec813fca8
Highlife1911 <Highlife1911@github.com>
2016-10-02 08:51:01 +0200

    Remove one unneded line.

--- gtest.cmake
+++ gtest.cmake
@@ -5,7 +5,6 @@ set(gtest_force_shared_crt ON CACHE BOOL "shared crt")
 
 add_subdirectory(gtest/googletest)
 
-set_property(TARGET gtest PROPERTY FOLDER "Tests" )
-set_property(TARGET gtest_main PROPERTY FOLDER "Tests" )
+set_property(TARGET gtest gtest_main PROPERTY FOLDER "Tests" )
 
 endmacro(add_gtest)
